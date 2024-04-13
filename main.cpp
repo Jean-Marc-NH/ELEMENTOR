7662232aa8f5368852600e338bf000a53c162fd2
Jean Marc Nadeau Huamani <101827583+Jean-Marc-NH@users.noreply.github.com>
2024-04-12 21:31:40 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,175 +1,139 @@
-template<class T = int>
-struct nodo {
-    T valores[10];
-    nodo<T>* next;
-    T* p;
-    nodo(T v, nodo<T>* n = NULL) {
-        p = valores;
-        *p = v;
-        next = n;
+#include <iostream>
+
+using namespace std;
+
+struct Nodo {
+    int* data;
+    Nodo* next;
+    int index;
+
+    Nodo() {
+        data = new int[10];
+        next = nullptr;
+        index = 0;
     }
 };
 
-template<class T = int>
-struct LLO {
-    nodo<T>* head = NULL;
-    nodo<T>* act = head;
-    nodo<T>* tail = NULL;
-    T* top = NULL;
-    T* p = NULL;
-    bool find(T v);
-    void add(T v);
-    void del(T v);
-    void print();
+struct Elementor {
+    Nodo* head;
+
+    Elementor() {
+        head = nullptr;
+    }
+
+    void add(int value) {
+        Nodo* curr = head;
+        Nodo* prev = nullptr;
+        while (curr != nullptr && curr->index == 10) {
+            prev = curr;
+            curr = curr->next;
+        }
+        if (curr == nullptr) {
+            curr = new Nodo;
+            curr->next = head;
+            head = curr;
+        }
+        int* ptr = curr->data;
+        while (ptr < curr->data + curr->index && *ptr < value) {
+            ptr++;
+        }
+        for (int* i = curr->data + curr->index; i > ptr; i--) {
+            *i = *(i - 1);
+        }
+        *ptr = value;
+        curr->index++;
+    }
+
+    void del(int value) {
+        Nodo* prev = nullptr;
+        Nodo* curr = head;
+        while (curr != nullptr) {
+            int* ptr = curr->data;
+            while (ptr < curr->data + curr->index && *ptr != value) {
+                ptr++;
+            }
+            if (ptr < curr->data + curr->index) {
+                for (int* i = ptr; i < curr->data + curr->index - 1; i++) {
+                    *i = *(i + 1);
+                }
+                curr->index--;
+                if (curr->index == 0) {
+                    if (prev == nullptr) {
+                        head = curr->next;
+                    }
+                    else {
+                        prev->next = curr->next;
+                    }
+                    Nodo* temp = curr;
+                    curr = curr->next;
+                    delete[] temp->data;
+                    delete temp;
+                }
+                else {
+                    curr = curr->next;
+                }
+                return;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    bool find(int value, Nodo** result) {
+        Nodo* curr = head;
+        while (curr != nullptr) {
+            int* ptr = curr->data;
+            while (ptr < curr->data + curr->index && *ptr != value) {
+                ptr++;
+            }
+            if (ptr < curr->data + curr->index) {
+                *result = curr;
+                return true;
+            }
+            curr = curr->next;
+        }
+        return false;
+    }
+
+    void print() {
+        Nodo* curr = head;
+        while (curr != nullptr) {
+            int* ptr = curr->data;
+            while (ptr < curr->data + curr->index) {
+                cout << *ptr << " ";
+                ptr++;
+            }
+            cout << "->";
+            curr = curr->next;
+        }
+    }
 };
 
 int main() {
-    LLO<int> l;
-    l.add(10);
-    l.add(12);
-    l.add(1);
-    l.add(13);
-    l.add(18);
-    l.add(17);
-    l.add(15);
-    l.add(4);
-    l.add(132);
-    l.add(121);
-    l.add(-1);
-    l.add(-13);
-    l.add(-18);
-    l.add(817);
-    l.add(915);
-    l.add(-4);
-
-    l.print();
-
-    l.del(4);
-
-    l.print();
-
-    return 0;
-}
-
-template<class T>
-void LLO<T>::add(T v) {
-    if (!head) {
-        head = new nodo<T>(v);
-        tail = head;
-        top = head->p;
+    Elementor list;
+    list.add(5);
+    list.add(3);
+    list.add(8);
+    list.add(1);
+    list.add(12);
+    list.add(13);
+    list.add(14);
+    list.add(15);
+    list.add(18);
+    list.add(19);
+    list.add(-1);
+    list.add(-5);
+    list.print(); 
+    cout << "\n";
+    list.del(3);
+    list.print();
+    cout << "\n";
+    Nodo* found = nullptr;
+    if (list.find(100, &found)) {
+        cout << "Found: " << *(found->data + 1) << endl; 
     }
     else {
-        if (!find(v)) {
-            if (v < *p) {
-                if (top == ((act->p) + 9)) {
-                    tail->next = new nodo<T>(v, tail->next);
-                    tail = tail->next;
-                    top = tail->p;
-                }
-                T tmp1 = *p;
-                T tmp2 = *(p + 1);
-                T* f = p;
-                while (f <= top) {
-                    if (f == ((act->p) + 9)) {
-                        act = act->next;
-                        f = act->p;
-                    }
-                    else {
-                        f++;
-                    }
-                    tmp2 = *f;
-                    *f = tmp1;
-                    tmp1 = tmp2;
-                }
-                top++;
-                *p = v;
-                if (*top < *(act->p))top--;
-            }
-            else {
-                if (p == ((act->p) + 9)) {
-                    tail->next = new nodo<T>(v, tail->next);
-                    tail = tail->next;
-                    top = tail->p;
-                }
-                top++;
-                p = top;
-                *p = v;
-            }
-        }
+        cout << "Element not found" << endl;
     }
-}
-
-template<class T>
-bool LLO<T>::find(T v) {
-    if (!head) return false;
-    act = head;
-    p = act->p;
-    while (p) {
-        if (v > *top) return false;
-        if (v < *p) return false;
-        if (*p == v) return true;
-        if (p == ((act->p) + 9)) {
-            act = act->next;
-            p = act->p;
-        }
-        else {
-            p++;
-        }
-    }
-    return false;
-}
-
-template<class T>
-void LLO<T>::del(T v) {
-    T* q = p;
-    if (find(v)) {
-        while (act->next) {
-            if (p != (act->p + 9)) {
-                q++;
-                *p = *q;
-                p++;
-            }
-            else {
-                if (act->next) {
-                    act = act->next;
-                    q = act->p;
-                    *p = *q;
-                    p = q;
-                }
-                else {
-                    p--;
-                    top = p;
-                }
-            }
-            if (p == top && act->p != p) {
-                p--;
-                p = top;
-            }
-        }
-        if (p == (act->p) && act->next) {
-            T* nextP = act->next->p;
-            *p = *nextP;
-            p = nextP;
-        }
-    }
-}
-
-template<class T>
-void LLO<T>::print() {
-    T* tmp = head->p;
-    nodo<T>* TMP = head;
-    while (tmp <= top) {
-
-        if (tmp == (TMP->p + 9)) {
-            TMP = TMP->next;
-            tmp = TMP->p;
-            cout << "] -> ";
-        }
-        if (tmp == (TMP->p)) {
-            cout << "[ ";
-        }
-        cout << *tmp << ",";
-        tmp++;
-    }
-
+    return 0;
 }
